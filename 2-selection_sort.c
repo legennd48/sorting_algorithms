27d4651a265735878b36b64d39a23aa9815cfe5c
Adewale Liasu <132359431+legennd48@users.noreply.github.com>
2023-10-21 15:36:29 +0000

    initial commit

---
+++ 2-selection_sort.c
@@ -0,0 +1,30 @@
+#include "sort.h"
+/**
+ * selection_sort - sorts an array of integers in ascending order
+ * using the Selection sort algorithm
+ * @array: array to be sorrted
+ * @size: size of array
+ */
+
+void selection_sort(int *array, size_t size)
+{
+	size_t i, j, min;
+	int temp;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		min  = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[min])
+				min = j;
+		}
+		if (min != i)
+		{
+			temp = array[min];
+			array[min] = array[i];
+			array[i] = temp;
+			print_array(array, size);
+		}
+	}
+}
---
+++ 3-quick_sort.c
@@ -0,0 +1,80 @@
+#include "sort.h"
+/**
+ * quick_sort - sorts an array of integers in ascending
+ * order using the Quick sort algorithm
+ * @array: array to be sorted
+ * @size: size of array
+ */
+
+void quick_sort(int *array, size_t size)
+{
+	if (array == NULL || size <= 1)
+		return;
+
+	/* Call the recursive quicksort function */
+	quicksort(array, 0, size - 1, size);
+}
+
+
+/**
+ * partition_lomuto - Partition the array using Lomuto scheme.
+ * @array: Array to be partitioned.
+ * @low: Starting index of the partition.
+ * @high: Ending index of the partition.
+ * @size: Size of the array.
+ * Return: Index of the pivot element after partition.
+ */
+int partition_lomuto(int *array, int low, int high, size_t size)
+{
+	int pivot = array[high]; /* Choose the pivot as the last element */
+	int i = low - 1, temp, j;
+
+	for (j = low; j <= high - 1; j++)
+	{
+		if (array[j] < pivot)
+		{
+			i++;
+			if (i != j)
+			{
+				/* Swap array[i] and array[j] */
+				temp = array[i];
+				array[i] = array[j];
+				array[j] = temp;
+				print_array(array, size);
+			}
+		}
+	}
+
+	/* Swap array[i + 1] and array[high] (pivot) */
+	if (i + 1 != high)
+	{
+		temp = array[i + 1];
+		array[i + 1] = array[high];
+		array[high] = temp;
+		print_array(array, size);
+	}
+
+	return (i + 1); /* Return the index of the pivot element */
+}
+
+/**
+ * quicksort - Recursive function to sort the partitioned array.
+ * @array: Array to be sorted.
+ * @low: Starting index of the partition.
+ * @high: Ending index of the partition.
+ * @size: Size of the array.
+ */
+void quicksort(int *array, int low, int high, size_t size)
+{
+	int pi;
+
+	if (low < high)
+	{
+		/* Partition the array and get the index of the pivot element */
+		pi = partition_lomuto(array, low, high, size);
+
+		/* Recursive calls to sort the sub-arrays */
+		quicksort(array, low, pi - 1, size);
+		quicksort(array, pi + 1, high, size);
+	}
+}
